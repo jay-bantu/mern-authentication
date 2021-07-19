f3a448de9859c8571c5b198f38917ba49dd86864
jay <jayrgeezy@gmail.com>
2021-07-19 14:48:55 +0300

    login logic, token setup, custom error handlers, authorization and authentication checkup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
-const User = require('../models/User')
+const User = require('../models/User');
+const ErrorResponse = require('../utils/errorResponse');
 
 exports.register = async (req,res,next)=>{
     const {username, email,password}= req.body;
@@ -8,25 +9,56 @@ exports.register = async (req,res,next)=>{
             username, email, password
         });
 
-        res.status(201).json({
-            success:true,
-            user
-        })
+        sendToken(user, 201, res);
     } catch (error) {
-        res.status(500).json({
-            success:false,
-            error:error.message
-        })
+       next(error);
         
     }
 
     }
-exports.login =(req,res,next)=>{
-    res.send('login Route');
+exports.login = async (req,res,next)=>{
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return next(new ErrorResponse("please provide email and password", 503));
+    }
+
+    try {
+        const user = await User.findOne({email}).select("+password")
+
+        if(!user){
+            return next(new ErrorResponse("Invalid Credentials", 401));
+
+        }
+
+        const isMatch = await user.matchPasswords(password);
+
+        if(!isMatch){
+            return next(new ErrorResponse("Invalid Credentials", 401));
+
+        }
+
+        
+        sendToken(user, 200, res);
+
+
+        
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            error: error.message
+        });
+        
+    }
 }
 exports.forgotpassword =(req,res,next)=>{
     res.send('Forgot Password Route');
 }
 exports.resetpassword =(req,res,next)=>{
     res.send('Reset Password Route');
+}
+
+const sendToken = (user, statusCode, res)=>{
+    const token =user.getSignedToken()
+    res.status(statusCode).json({success:true, token})
 }
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const  mongoose = require('mongoose');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -36,6 +37,14 @@ UserSchema.pre('save', async function (next){
     this.password =await bcrypt.hash(this.password, salt);
     next();
 })
+
+UserSchema.methods.matchPasswords = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+UserSchema.methods.getSignedToken = function (){
+    return jwt.sign({id:this._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRE});
+}
 
 
 const User =mongoose.model('Users', UserSchema);
